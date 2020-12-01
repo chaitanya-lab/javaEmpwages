2001666813159314aa72d971099ba2529b3edd05
unknown <you@example.com>
2020-12-01 10:55:44 +0530

    Added working parttime file

--- EmpWages.java
+++ EmpWages.java
@@ -18,14 +18,21 @@ public class EmpWages {
         // TODO code application logic here
         System.out.println("Welcome to Employe wages computation");
         int Full_time=1;
+        int Part_Time=2;
         int Wage_per_hr=20;
         int working_hr_perday=8;
-         double empCheck=Math.floor((Math.random()*10)%2);
+        int working_parttime=8;
+         double empCheck=Math.floor((Math.random()*10)%3);
          if (Full_time==empCheck){
              System.out.println("Employee is  present");
             int Total_wageper_day=( Wage_per_hr*working_hr_perday);
             System.out.println(Total_wageper_day);
              
+         }
+         else if (Part_Time==empCheck){
+             System.out.println("Employee is working parttime");
+             int Total_part_timewage=(Wage_per_hr*working_parttime);
+             System.out.println(Total_part_timewage);
          }
          else
              System.out.println("Employee is absent");
