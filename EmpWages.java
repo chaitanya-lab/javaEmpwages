8251aba7a1367b9650028a1930d8aae236bbda65
unknown <you@example.com>
2020-12-03 10:52:24 +0530

    Added new file

--- EmpWages.java
+++ EmpWages.java
@@ -14,43 +14,31 @@ public class EmpWages {
     /**
      * @param args the command line arguments
      */
-    public static final int Full_time=1;
-    public static final int Part_Time=2;
-    public static final int Wage_per_hr=20;
-    public static final int Working_day_permonth=20;
-    public static int computeEmpwage(){
-        System.out.println("Welcome to Employe wages computation");
-        int maxHrsInmonth=100;
-        int TotalempHr=0;
-        int TotalWorkingDay=0;
-        int empHrs=0,salary=0;
-        while( TotalempHr <= maxHrsInmonth && TotalWorkingDay <= Working_day_permonth ){
-        TotalWorkingDay++;
-        int empCheck=(int)Math.floor((Math.random()*10)%3);
-        switch(empCheck){
-                case 1:
-                    empHrs=8;
-                     break;
-                case 2:
-                    empHrs=4;
-                     break;
+  
+         public static void CalculateWage(int WagePerHr ,int WorkPerDay,int WorkingDays){
+        int TotalEmpHr = 0;
+        int TotalWorkingDay = 1;
+        int MxHrsInMonth = 100;
+        int WagePerDay = 0;
 
-                default:
-                    empHrs=0;
-                     break;
+        while (TotalEmpHr < MxHrsInMonth && TotalWorkingDay <= WorkingDays) {
 
+            WagePerDay = (WagePerHr * WorkPerDay);
+            System.out.println("The Day "+TotalWorkingDay+" Wage Of Employee Is :" + WagePerDay);
+            TotalWorkingDay++;
         }
-        TotalempHr=(TotalempHr+empHrs);
-        int salary_per_day=(empHrs*Wage_per_hr);
-        System.out.println(salary_per_day);
-        salary=(TotalempHr*Wage_per_hr);
-      }
-        System.out.println("totoal salary :" +salary);
-        return(salary);
-   }     
+        int MonthlyWage = (WagePerDay * TotalWorkingDay);
+         
+        System.out.println("The Monthly Wage Of Employee Is :" + MonthlyWage);
+         }
+
+     
 
         public static void main(String[] args) {
         // TODO code application logic here
-        computeEmpwage();  
+          CalculateWage(20,2,10);
+        CalculateWage(10,4,20);
+        CalculateWage(10,4,20);
+        CalculateWage(10,4,20); 
     }
 }
